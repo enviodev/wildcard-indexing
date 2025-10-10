4029b397a88d5f25e884ae871639a5de822745cd
Nikhil Bhintade <29760727+nikbhintade@users.noreply.github.com>
2025-10-10 05:35:31 +0000

    feat: wildcard indexing template (v1)

---
+++ src/EventHandlers.ts
@@ -0,0 +1,15 @@
+import { ERC20 } from "generated";
+
+ERC20.Transfer.handler(
+  async ({ event, context }) => {
+    context.Transfer.set({
+      id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
+      amount: event.params.value,
+      from: event.params.from,
+      to: event.params.to,
+      contract: event.srcAddress,
+      transaction: event.transaction.hash,
+    });
+  },
+  { wildcard: true },
+);
